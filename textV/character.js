2f18799fcdeb5f31e17400703e83089ca7a50cf9
LouisDecaudaveine <tz20989@bristol.ac.uk>
2021-12-17 13:19:02 +0000

    Poopy di scoop text sample

--- textV/character.js
+++ textV/character.js
@@ -1,10 +1,16 @@
 function Character(){
   this.charString = 'b'
-  this.pos = 0
+  this.pos = {x: 0, y: 0}
   this.funcList = {}
+  this.relX = 0
+  this.relY = 0
 
   this.setPos = (position) => {
     this.pos = position
+  }
+  this.setRel = (x, y) => {
+    this.relX = x
+    this.relY = y
   }
 
   this.setChar = (character) => {
@@ -12,86 +18,105 @@ function Character(){
   }
 
   this.getCharFunc = () => {
-    this.funcList[this.charString]()
+    if(this.charString == ' ')this.funcList['space']();
+    else if(this.charString == '.')this.funcList['dot']();
+    else if(this.charString == ',')this.funcList['comma']();
+    else {this.funcList[this.charString]()}
+    strokeWeight(0)
+    rect(this.pos.x, this.pos.y, this.relX, this.relY)
+  }
+
+  this.setPos = (x, y) => {
+    this.pos.x = x
+    this.pos.y = y
   }
 
   //EACH CHARACTER REP
   this.funcList.a = () => {
-    ellipse(200,200,100,100)
+    fill('blue')
   }
   this.funcList.b = () => {
-    console.log("b")
+    fill('red')
   }
   this.funcList.c = () => {
-    console.log("c")
+    fill("orange")
   }
   this.funcList.d = () => {
-    console.log("d")
+    fill("purple")
   }
   this.funcList.e = () => {
-    console.log("e")
+    fill('yellow')
   }
   this.funcList.f = () => {
-    console.log("f")
+    fill("magenta")
   }
   this.funcList.g = () => {
-    console.log("g")
+    fill("green")
   }
   this.funcList.h = () => {
-    console.log("h")
+    fill("cyan")
   }
   this.funcList.i = () => {
-    console.log("i")
+    fill(230,170,30)
   }
   this.funcList.j = () => {
-    console.log("j")
+    fill("black")
   }
   this.funcList.k = () => {
-    console.log("k")
+    fill(134,23,234)
   }
   this.funcList.l = () => {
-    console.log("l")
+    fill(34,123,234)
   }
   this.funcList.m = () => {
-    console.log("m")
+    fill(34,123,34)
   }
   this.funcList.n = () => {
-    console.log("n")
+    fill(212,123,34)
   }
   this.funcList.o = () => {
-    console.log("o")
+    fill(2,123,34)
   }
   this.funcList.p = () => {
-    console.log("p")
+    fill(2,223,234)
   }
   this.funcList.q = () => {
-    console.log("q")
+    fill(66,166,255)
   }
   this.funcList.r = () => {
-    console.log("r")
+    fill(66,166,255)
   }
   this.funcList.s = () => {
-    console.log("s")
+    fill(30,240,70)
   }
   this.funcList.t = () => {
-    console.log("t")
+    fill(227, 165, 56)
   }
   this.funcList.u = () => {
-    console.log("u")
+    fill(69,42,096)
   }
   this.funcList.v = () => {
-    console.log("v")
+    fill(100,255,255)
   }
   this.funcList.w = () => {
-    console.log("w")
+    fill(204, 197, 144)
   }
   this.funcList.x = () => {
     console.log("x")
   }
   this.funcList.y = () => {
-    console.log("y")
+    fill(57, 204, 184)
   }
   this.funcList.z = () => {
     console.log("a")
+  }
+  this.funcList.space = () => {
+    fill(143,166,255)
+  }
+  this.funcList.dot = () => {
+    console.log(".")
+  }
+  this.funcList.comma = () => {
+    console.log(",")
   }
 }
--- textV/sketch.js
+++ textV/sketch.js
@@ -8,22 +8,64 @@
 //first split the image as equally as possible for each charater to be represented
 //then certain characters can add effects, or certain characters at a certain point in the text(ex: the characters which are prime numbers add effects)
 
+let text = "Poopy di scoop Scoop diddy whoop Whoop di coop di poop Poop di scoopty Scoopty whoop Whoopity scoop  whoop poop Poop diddy whoop scoop Poop poop Scoop diddy whoop Whoop diddy scoop Whoop diddy scoop poop"
+
+
 let char = new Character()
-let keyy = ''
 let charCount = 0
+text = text.toLocaleLowerCase()
+let chara = []
 
 function setup() {
-  createCanvas(400, 400);
-}
+  createCanvas(windowWidth, windowHeight);
+  let bestFactors = factorFinder(text.length)
+  let relX = Math.floor(width/bestFactors[0])
+  let relY = Math.floor(height/bestFactors[1])
+  
+  for(let i = 0; i<text.length;i++){
+    chara[i] = new Character()
+    chara[i].setChar(text[i])
+    chara[i].setRel(relX, relY)
+  }
 
-function keyTyped(){
-  keyy = key
-  charCount++
+  for (let x = 0; x < bestFactors[0]; x++){
+    for(let y = 0; y < bestFactors[1]; y++){
+      let index = x*bestFactors[1] + y
+      if(index < text.length){
+        chara[index].setPos(x*relX, y*relY)
+      }
+    }
+  }
 }
-
 
 function draw() {
-  background(220);
-  char.setChar(key)
-  if (charCount>0)char.getCharFunc();
+  background(255);
+  for(let i = 0; i<text.length;i++){
+    chara[i].getCharFunc();
+  }
+  noLoop()
 }
+
+function factorFinder(num) {
+  let smallestCouple = []
+  let factors = []
+  for(let i = 0; i<num; i++){
+    if(num%i == 0){
+      let otherF = num/i
+      factors.push([i,otherF])
+    }
+  }
+  let smallestDif = num
+  for(let x=0; x<factors.length;x++){
+    let dif = Math.abs(factors[x][0]-factors[x][1])
+    if(dif < smallestDif) {
+      smallestCouple = factors[x];
+      smallestDif = dif
+    }
+  }
+  if (factors.length  == 2) {
+    smallestCouple = factorFinder(num+1)
+  }
+  return smallestCouple
+}
+ 
