be42cfa8eb8215129774472bcee0a7189987307d
LouisDecaudaveine <tz20989@bristol.ac.uk>
2022-01-30 00:16:17 +0000

    made volume rep look better

--- soundStudies/sketch.js
+++ soundStudies/sketch.js
@@ -2,25 +2,38 @@ let moosic
 let pause = 2
 let amp
 let vol 
+let y 
+let x = 0
+let inc = 0
 function preload(){
+  y = 100
   soundFormats("mp3")
   moosic = loadSound("./Breaka - We Move - 08 Solaria.mp3")
 }
 
 function setup() {
-  let can = createCanvas(400, 400);
+  let can = createCanvas(400, 4000);
   can.mousePressed(canvasPressed)
   amp = new p5.Amplitude()
+  background(200)
 }
 
 function draw() {
-  
-  background(50);
-  vol = amp.getLevel()
-  let diag = map(vol, 0,1, 0,200)
   fill(100,255,100)
-  ellipse(width/2, height/2 , diag, diag)
+  strokeWeight(3)
+  vol = amp.getLevel()
 
+  let prevX = x
+  x++
+  let prevY = y 
+  y = map(vol, 0,1, 0+inc,100+inc)
+  
+  // point(x, height-y) 
+  line(prevX,prevY, x,y)
+  if (x>400){
+    x=0
+    inc+= 50
+  }
 }
 
 function canvasPressed(){
